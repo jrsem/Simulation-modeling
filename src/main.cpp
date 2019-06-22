da3d3930c0226680517d4fd4362e4596fd6ff5a7
CobraMJD <42187314+CobraMJD@users.noreply.github.com>
2019-06-22 19:14:36 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,40 @@
 #include "Team9.h"
+
+using namespace std;
 int main()
 {
 	Team9 *team9 = new Team9();
 	//	team9->showlist();
-        double err = team9->halfWidthConfidenceInterval(0.80);
-        cout<<"erroPAdrao: "<<err<<endl;
+        cout<<"Main Test"<<endl;
 	team9->average();
 	team9->max();
 	team9->min();
 	team9->mediane();
-        team9->quartil(4);
-        team9->quantile(2,4);
-        team9->centil(25);
-        team9->decil(2);
+        double q = team9->quartil(3);
+         cout<<"quartil 3: "<<q<<endl;
+       // team9->quantile(2,4);
+        q = team9->centil(25);
+          cout<<"centil 25: "<<q<<endl;
+        q = team9->decil(2);
+          cout<<"decil 2: "<<q<<endl;
 	 team9->mode();
 	 team9->variance();
 	// team9->splitList();
         team9->histogramClassLowerLimit(2);
         team9->histogramClassLowerLimit(3);
         team9->histogramClassLowerLimit(2);
-        team9->stddeviation();
-        team9->variationCoef();
-        team9->newSampleSize(0.95, 5);
-        team9->histogramNumClasses();
-        team9->histogramClassLowerLimit(3);
-        team9->histogramClassFrequency(3);
-        team9->halfWidthConfidenceInterval(0.95);
+        q = team9->stddeviation();
+         cout<<"stddeviation: "<<q<<endl;
+        q=team9->variationCoef();
+         cout<<"variationCoef: "<<q<<endl;
+        q=team9->newSampleSize(0.95, 5);
+         cout<<"newSampleSize: "<<q<<endl;
+        q=team9->histogramNumClasses();
+         cout<<"numClasses: "<<q<<endl;
+        q=team9->histogramClassFrequency(3);
+         cout<<"frequency class 3: "<<q<<endl;
+        q=team9->halfWidthConfidenceInterval(0.95);
+         cout<<"erroPAdrao(.95): "<<q<<endl;
 	delete team9;
 
 	return 0;
