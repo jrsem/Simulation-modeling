8c883c75098e036bcc2cd5636e3c254a609d59a1
CobraMJD <42187314+CobraMJD@users.noreply.github.com>
2019-06-22 02:48:45 -0300

    All methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,31 @@
-#include "Team-9.h"
+#include "Team9.h"
 int main()
 {
 	Team9 *team9 = new Team9();
 	//	team9->showlist();
+        double err = team9->halfWidthConfidenceInterval(0.80);
+        cout<<"erroPAdrao: "<<err<<endl;
 	team9->average();
 	team9->max();
 	team9->min();
-	//team9->mediane();
-	// team9->mode();
-	// team9->variance();
+	team9->mediane();
+        team9->quartil(4);
+        team9->quantile(2,4);
+        team9->centil(25);
+        team9->decil(2);
+	 team9->mode();
+	 team9->variance();
 	// team9->splitList();
+        team9->histogramClassLowerLimit(2);
+        team9->histogramClassLowerLimit(3);
+        team9->histogramClassLowerLimit(2);
+        team9->stddeviation();
+        team9->variationCoef();
+        team9->newSampleSize(0.95, 5);
+        team9->histogramNumClasses();
+        team9->histogramClassLowerLimit(3);
+        team9->histogramClassFrequency(3);
+        team9->halfWidthConfidenceInterval(0.95);
 	delete team9;
 
 	return 0;
